bf64cff51db9b39d35a6302680a844134753d63e
DESKTOP-SR275TT\Playdata <99dlatjdwo@gmail.com>
2025-02-07 17:57:16 +0900

    basetimeentity 추가

--- src/main/java/com/beyond/StomachForce/Common/domain/BaseTimeEntity.java
+++ src/main/java/com/beyond/StomachForce/Common/domain/BaseTimeEntity.java
@@ -1,19 +1,4 @@
 package com.beyond.StomachForce.Common.domain;
 
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
-
-@MappedSuperclass
-@Getter
 public class BaseTimeEntity {
-    @CreationTimestamp
-    private LocalDateTime createdTime;
-    @UpdateTimestamp
-    private LocalDateTime updatedTime;
-    @UpdateTimestamp
-    private LocalDateTime lastLogIn;
 }
