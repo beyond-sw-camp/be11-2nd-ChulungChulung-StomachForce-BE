7ba7c627f5f4e4f2395b0c89e4e004d9a2dd8b42
01youngil <cka1782@gmail.com>
2025-02-07 17:23:51 +0900

    menu

---
+++ src/main/java/com/beyond/StomachForce/allergyInfo/domain/Peanut.java
@@ -0,0 +1,5 @@
+package com.beyond.StomachForce.allergyInfo.domain;
+
+public enum Peanut {
+    Y,N
+}
---
+++ src/main/java/com/beyond/StomachForce/menu/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.beyond.StomachForce.menu.repository;
+
+import com.beyond.StomachForce.menu.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu,Long> {
+}
