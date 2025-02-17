809e34dc2639b49b5cea40d6c3dcd502bc904a95
gimpitang <dudwodpdy@gmail.com>
2025-02-17 16:03:30 +0900

    debug&develop

--- src/main/java/com/beyond/StomachForce/restaurant/dtos/forRestaurantInfo/RestaurantInfoListRes.java
+++ src/main/java/com/beyond/StomachForce/restaurant/dtos/forRestaurantInfo/RestaurantInfoListRes.java
@@ -16,7 +16,6 @@ public class RestaurantInfoListRes {
     private RestaurantInfoStatus status;
 
 
-    //      인텔리제이랑 지피티가 둘 다 만들으래요...dto에서 생성자 만드는게 불변성을 보장하기때문이립니다.
     public RestaurantInfoListRes(Long id, String informationText, RestaurantInfoStatus restaurantInfoStatus, LocalDateTime createdTime) {
     }
 }
